63ebede049e2221dac03c6cfd52f434e3238c4f9
Conngas <conngas@outlook.com>
2023-09-19 09:46:16 +0800

    Spark Log Output

--- Spark/src/Spark/Core/Log.cpp
+++ Spark/src/Spark/Core/Log.cpp
@@ -1,7 +1,8 @@
 #include "spkpch.h"
 #include "Spark/Core/Log.h"
 
-#include "spdlog/sinks/stdout_color_sinks.h"
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Spark {
 	// 保留Sharedptr，因为不是Spark的资产内容
@@ -10,14 +11,25 @@ namespace Spark {
 
 	void Log::Init()
 	{
-		// 设置语句log格式，参考 https://github.com/gabime/spdlog/wiki/1.-QuickStart
-		spdlog::set_pattern("%^[%T] %n: %v%$");
-
+		std::vector<spdlog::sink_ptr> logSinks;
 		// 核心代码颜色等级设置
-		s_CoreLogger = spdlog::stdout_color_mt("Spark");
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Spark.log",true));
+
+		// 设置语句log格式，参考 https://github.com/gabime/spdlog/wiki/1.-QuickStart
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%1] %n: %v");
+
+		// 核心部分代码控制
+		s_CoreLogger = std::make_shared<spdlog::logger>("Spark", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_CoreLogger);
 		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
+		
 		// 客户端代码设置
-		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_ClientLogger);
 		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
 }
