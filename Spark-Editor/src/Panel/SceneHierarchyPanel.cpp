3118b93909f1ca57f5f40c39d96524362de3ef91
Conngas <conngas@outlook.com>
2023-10-17 17:01:18 +0800

    Update Propertes Panel

--- Spark-Editor/src/Panel/SceneHierarchyPanel.cpp
+++ Spark-Editor/src/Panel/SceneHierarchyPanel.cpp
@@ -2,6 +2,7 @@
 #include "Spark/Scenes/Component.h"
 
 #include <imgui/imgui.h>
+#include <glm/gtc/type_ptr.hpp>
 
 namespace Spark {
 
@@ -17,6 +18,9 @@ namespace Spark {
 
 	void SceneHierarchyPanel::OnImGuiRender()
 	{
+		//////////////////////////////////////////////////////////////////////////
+		/// Hierarchy
+		//////////////////////////////////////////////////////////////////////////
 		ImGui::Begin("Scene Hierarchy");
 		// 接收lambda函数并对每个entity遍历，[&]捕获子句
 		m_Context->m_Registry.each([&](auto entityID)
@@ -24,6 +28,17 @@ namespace Spark {
 				Entity entity{ entityID, m_Context.get() };
 				DrawEntityNode(entity);
 			});
+
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+			m_SelectionContext = {};
+		ImGui::End();
+
+		//////////////////////////////////////////////////////////////////////////
+		/// Property
+		//////////////////////////////////////////////////////////////////////////
+		ImGui::Begin("Properties");
+		if (m_SelectionContext)
+			DrawComponent(m_SelectionContext);
 		ImGui::End();
 	}
 
@@ -45,6 +60,34 @@ namespace Spark {
 			if (opened)
 				ImGui::TreePop();
 			ImGui::TreePop();
+		}
+	}
+
+	void SceneHierarchyPanel::DrawComponent(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+			// 用于命名缓冲区
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			// 设置唯一树节点序号用于打开列表
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+				// 
+				ImGui::TreePop();
+			}
 		}
 	}
 }
--- Spark-Editor/src/Panel/SceneHierarchyPanel.h
+++ Spark-Editor/src/Panel/SceneHierarchyPanel.h
@@ -17,6 +17,7 @@ namespace Spark {
 		void OnImGuiRender();
 	private:
 		void DrawEntityNode(Entity entity);
+		void DrawComponent(Entity entity);
 	private:
 		Ref<Scene> m_Context;
 		Entity m_SelectionContext;
